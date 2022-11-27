1d401abf527a833923c0144e53c9a09001582efb
Mark Hedley Jones <MarkHedleyJones@gmail.com>
2022-11-27 16:49:45 +0900

    Increment patch version

--- io_pcd/__init__.py
+++ io_pcd/__init__.py
@@ -32,7 +32,7 @@ bl_info = {
     "doc_url": "",
     "location": "File > Import/Export > PCD (.pcd)",
     "name": "Point Cloud Format (.pcd)",
-    "version": (1, 5, 0),
+    "version": (1, 5, 1),
     "warning": "",
 }
 
